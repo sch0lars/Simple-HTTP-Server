2383b1431cd135040d08464921b061d0b750936c
sch0lars <sch0lars@users.noreply.github.com>
2023-09-09 20:37:49 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simple-HTTP-Server
+A simple HTTP server written in Node.js
+
+[![Node.js](https://img.shields.io/badge/_-Node.js-025800?logo=Node.JS)](https://nodejs.org/)
+![Command Line Interface](https://img.shields.io/badge/_-Command_Line_Interface-black?logo=Windows%20Terminal&logoColor=black&labelColor=708091)
+[![Made with GNU Emacs](https://img.shields.io/badge/built_with-GNU_Emacs-aa69d1?logo=GNU%20Emacs&logoColor=aa69d1)](https://www.gnu.org/software/emacs/)
+
+## Installation
+
+To install the simple HTTP server, ensure that [Node.js](https://nodejs.org/) is installed and then clone the repo:
+
+`https://github.com/sch0lars/Simple-HTTP-Server.git`
+
+After cloning the repo, refer to the `Usage` section for running the server.
+
+## Usage
+
+```
+Usage: node simple_http_server.js [-d directory] [-p port]
+ -d, --directory <directory>    the directory from which to serve the files (default: current directory)
+ -p, --port <port>              the port number on which the HTTP service is run (default: 8080)
+```
+ 
+ The usage instructions can also be accessed by using `simple_http_server.js -h` or `simple_http_server.js --help`
+ 
+## License
+ [GNU General Public License](https://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ simple_http_server.js
@@ -0,0 +1,164 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { exit } = require('process');
+
+// Get the command line arguments
+args = process.argv;
+
+// Display usage if the help flag is set
+if (args.indexOf('-h') > -1 || args.indexOf('--help') > -1) {
+    console.log(`Usage: ${args[0].split('/').at(-1)} ${args[1].split('/').at(-1)} [-d directory] [-p port]`);
+    console.log(' -d, --directory <directory>\tthe directory from which to serve the files (default: current directory)');
+    console.log(' -p, --port <port>\t\tthe port number on which the HTTP service is run (default: 8080)');
+    exit(0);
+}
+
+// Set the host and port for the server
+const host = 'localhost';
+// This is the directory from which we will serve files
+var servingDirectory = './'
+// This is the port on which we will run the HTTP service
+var port = 8080;
+
+// Check for the port flags
+if (args.indexOf('-p') > -1 || args.indexOf('--port') > -1) {
+    let portArgument;
+    if (args.indexOf('-p') > -1) {
+	portArgument = args[args.indexOf('-p') + 1];
+    } else {
+	portArgument = args[args.indexOf('--port') + 1];
+    }
+    if (! portArgument) {
+	let timestamp = new Date().toISOString();
+	console.log(`[ERROR]\t\t${timestamp}\tThe port flag requires an argument`);
+	exit(1);
+    } else {
+	port = portArgument;
+    }
+}
+
+// Check for the directory flags
+if (args.indexOf('-d') > -1 || args.indexOf('--directory') > -1) {
+    let directoryArgument;
+    if (args.indexOf('-d') > -1) {
+	directoryArgument = args[args.indexOf('-d') + 1];
+    } else {
+	directoryArgument = args[args.indexOf('--directory') + 1];
+    }
+    if (! directoryArgument) {
+	let timestamp = new Date().toISOString();
+	console.log(`[ERROR]\t\t${timestamp}\tThe directory flag requires an argument`);
+	exit(1);
+    } else {
+	servingDirectory = directoryArgument;
+    }
+}
+    
+http.createServer((req, res) => {
+    // This will create the path for the files
+    const filePath = path.join(servingDirectory, req.url);
+
+    // Create a timestamp for logging
+    const timestamp = new Date().toISOString();
+    // Get the HTTP version
+    const httpRequestVersion = req.httpVersion;
+    // Log the request
+    console.log(`[REQUEST]\t${timestamp}\tHTTP/${httpRequestVersion} ${req.method} ${req.url}`);
+
+    // Get the HTTP response version
+    var httpResponseVersion = res.httpVersion;
+    
+    // Check if we are listing the directory or serving a file
+    fs.stat(filePath, (err, stats) => {
+	// Handle any errors
+        if (err) {
+	    const errorCode = err.statusCode ?? 500;
+            res.writeHead(errorCode, {
+                'Content-Type': 'text/plain'
+            });
+            res.end(`${filePath} not found`);
+	    // Log the error
+	    httpResponseVersion = httpRequestVersion ?? 1.1;	    
+            console.log(`[ERROR]\t\t${timestamp}\tHTTP/${httpResponseVersion} ${errorCode} ${err.message}`);
+        } else { // If there are no errors, serve the files
+	    // Check if we are serving a directory
+            if (stats.isDirectory()) {
+                // Display the list of files as an HTML page
+                fs.readdir(filePath, (err, files) => {
+		    // Handle any errors
+                    if (err) {
+			const errorCode = err.statusCode ?? 500;
+                        res.writeHead(errorCode, {
+                            'Content-Type': 'text/plain'
+                        });
+                        res.end('Internal Server Error');
+			// Log the error
+			httpResponseVersion = httpRequestVersion ?? 1.1;
+			console.log(`[ERROR]\t\t${timestamp}\tHTTP/${httpResponseVersion} ${errorCode} ${err.message}`);
+                    } else { // If there are no errors, serve the directory
+			// Generate an index of the directory
+                        res.writeHead(200, {
+                            'Content-Type': 'text/html'
+                        });
+                        res.write('<html><body>');
+                        res.write(`<h1>Index of ${req.url}</h1>`);
+                        res.write('<ul>');
+			// If we are not serving the root directory, create a link to the parent directory
+			if (req.url !== '/') {
+			    const parentDirectory = path.join(req.url, '..');
+			    res.write(`<li><a href="${parentDirectory}">../</a></li>`);
+			}
+                        files.forEach((file) => {
+			    const fileLink = path.join(req.url, file);
+                            res.write(`<li><a href="${fileLink}">${file}</a></li>`);
+                        });
+                        res.write('</ul>');
+                        res.write('</body></html>');
+                        res.end();
+
+			// Log the response
+			console.log(`[RESPONSE]\t${timestamp}\tHTTP/${httpRequestVersion} 200 OK`)
+                    }
+                });
+            } else { // Otherwise, we are serving a file
+                // Get the MIME type for the file based on the file extension
+                var contentType;
+                switch(path.extname(filePath)) {
+                case '.tar.gz':
+                    contentType = 'application/x-gzip';
+                    break;
+                default:
+                    contentType = 'text/plain';
+                }
+                // Set the appropriate content type in the response
+                res.writeHead(200, {
+                    'Content-Type': contentType
+                });
+                // Read the file data and serve it in the response
+                fs.readFile(filePath, (err, data) => {
+		    // Handle any errors
+                    if (err) {
+			const errorCode = err.statusCode ?? 500;
+                        res.writeHead(500, {
+                            'Content-Type': 'text/plain'
+                        });
+                        res.end('Internal Server Error');
+			// Log the error
+			httpResponseVersion = httpRequestVersion ?? 1.1;
+			console.log(`[ERROR]\t\t${timestamp}\tHTTP/${httpResponseVersion} ${errorCode} ${err.message}`);
+                    } else { // If there are no errors, serve the file
+                        res.end(data);
+			// Log the response
+			console.log(`[RESPONSE]\t${timestamp}\tHTTP/${httpRequestVersion} 200 OK`)
+                    }
+                });
+            }
+        }
+    });
+}).listen(port, () => {
+    // Create a timestamp for logging
+    const timestamp = new Date().toISOString();
+    console.log(`[INFO]\t\t${timestamp}\tServer is now running on ${host}:${port}`);
+    console.log(`[INFO]\t\t${timestamp}\tServing files from directory: ${servingDirectory}`);
+});
